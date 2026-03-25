eca175d1360bbd4ffa58968d76211dcefa411f42
nithinveshala <nithinveshala123@gmail.com>
2026-03-25 23:15:14 +0530

    cicd

--- vehicle_service/settings.py
+++ vehicle_service/settings.py
@@ -28,11 +28,7 @@ SECRET_KEY = os.getenv(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = [
-    host.strip()
-    for host in os.getenv('ALLOWED_HOSTS', '*').split(',')
-    if host.strip()
-]
+ALLOWED_HOSTS = ['*']
 
 CSRF_TRUSTED_ORIGINS = [
     origin.strip()
